6ce6ff0eee9644d2612d29cbc9742621f69dacca
x-shadow-x <2509344578@qq.com>
2017-06-14 23:39:45 +0800

    商品列表页商品列表静态布局

--- src/pages/brand/goods_list.vue
+++ src/pages/brand/goods_list.vue
@@ -19,55 +19,113 @@
 			<router-link to="/goods_search" class="search_btn"></router-link>
 		</div>
 		<div class="filter_bar">
-			<div class="sort_box">
-				<div class="sort_result_box" @click.stop="toggleSortList">
-					<span class="sort_result">{{sortResult}}</span>
-					<i class="arr_icon" :class="{active: toggleMap.isShowSortList}"></i>
-				</div>
-				<ul class="sort_list" v-if="toggleMap.isShowSortList">
-					<li v-for="(item, index) in sortList" class="sort_item" :class="{active: item.isSelected}" @click.stop="selectSortItem(index)">{{item.text}}</li>
-				</ul>
-			</div>
-			<div class="filter_box">
-				<div class="filter_btn_box" @click.stop="toggleFilterList">
-					<span class="filter_btn">筛选</span>
-					<i class="arr_icon" :class="{active: toggleMap.isShowFilterList}"></i>
-				</div>
+            <div class="filter_bar_box">
+                <span class="new_good_icon"></span>
+    			<div class="sort_box">
+    				<div class="sort_result_box" @click.stop="toggleSortList">
+    					<span class="sort_result">{{sortResult}}</span>
+    					<i class="arr_icon" :class="{active: toggleMap.isShowSortList}"></i>
+    				</div>
+    				<ul class="sort_list" v-if="toggleMap.isShowSortList">
+    					<li v-for="(item, index) in sortList" class="sort_item" :class="{active: item.isSelected}" @click.stop="selectSortItem(index)">{{item.text}}</li>
+    				</ul>
+    			</div>
+    			<div class="filter_box">
+    				<div class="filter_btn_box" @click.stop="toggleFilterList">
+    					<span class="filter_btn">筛选</span>
+    					<i class="arr_icon" :class="{active: toggleMap.isShowFilterList}"></i>
+    				</div>
 
-				<div class="filter_list_out_box" :class="{show: toggleMap.isShowFilterList}">
-					<div class="filter_list_box" @click.stop="">
-						<template v-if="colorPropertyListArray.length > 0 && goodPropertyListArray.length > 0">
-							<div class="filter_list">
-								<div class="prototype_box" v-for="(goodPropertyItem, goodPropertyIndex) in goodPropertyListArray" @click="handleSelectProp($event)">
-									<h2 class="prototype_title">{{goodPropertyItem.PropertyName}}</h2>
-									<ul class="prototype_list">
-										<li class="prototype_item" v-for="(item, index) in goodPropertyItem.GoodPropertyArray" :data-attr-id="item.attr_id" :data-property-index="goodPropertyIndex" :data-index="index">{{item.attr_value}}</li>
-									</ul>
-								</div>
-								<div class="prototype_box" v-if="colorPropertyListArray.length > 0" @click="handleSelectProp($event, 'color')">
-									<h2 class="prototype_title">颜色</h2>
-									<ul class="prototype_list">
-										<li class="prototype_item" v-for="item in colorPropertyListArray" :data-colorcat-id="item.ColorPropertyArray[0].colorcat_id">{{item.PropertyName}}</li>
-									</ul>
-								</div>
-							</div>
-							<div class="handle_btn_box">
-								<span class="btn" @click="resetFilter">重新筛选</span>
-								<span class="btn confirm_btn" @click="confirmFilter">确认</span>
-							</div>
-						</template>
-						<template v-else>
-							<i class="empty_filter_icon"></i>
-							<span class="empty_filter_tip">暂无筛选条件！</span>
-						</template>
-					</div>
-				</div>
-			</div>
+    				<div class="filter_list_out_box" :class="{show: toggleMap.isShowFilterList}">
+    					<div class="filter_list_box" @click.stop="">
+    						<template v-if="colorPropertyListArray.length > 0 && goodPropertyListArray.length > 0">
+    							<div class="filter_list">
+    								<div class="prototype_box" v-for="(goodPropertyItem, goodPropertyIndex) in goodPropertyListArray" @click="handleSelectProp($event)">
+    									<h2 class="prototype_title">{{goodPropertyItem.PropertyName}}</h2>
+    									<ul class="prototype_list">
+    										<li class="prototype_item" v-for="(item, index) in goodPropertyItem.GoodPropertyArray" :data-attr-id="item.attr_id" :data-property-index="goodPropertyIndex" :data-index="index">{{item.attr_value}}</li>
+    									</ul>
+    								</div>
+    								<div class="prototype_box" v-if="colorPropertyListArray.length > 0" @click="handleSelectProp($event, 'color')">
+    									<h2 class="prototype_title">颜色</h2>
+    									<ul class="prototype_list">
+    										<li class="prototype_item" v-for="item in colorPropertyListArray" :data-colorcat-id="item.ColorPropertyArray[0].colorcat_id">{{item.PropertyName}}</li>
+    									</ul>
+    								</div>
+    							</div>
+    							<div class="handle_btn_box">
+    								<span class="btn" @click="resetFilter">重新筛选</span>
+    								<span class="btn confirm_btn" @click="confirmFilter">确认</span>
+    							</div>
+    						</template>
+    						<template v-else>
+    							<i class="empty_filter_icon"></i>
+    							<span class="empty_filter_tip">暂无筛选条件！</span>
+    						</template>
+    					</div>
+    				</div>
+    			</div>
+            </div>
 		</div>
+
+        <div class="goods_list_box" id="goodsListBox">
+            <div class="scroller">
+                <ul class="goods_list">
+                    <li class="goods_item" v-for="">
+                        <div class="img_box">
+                            <img src="../../images/test_img.jpg" alt="商品名字" class="goods_img">
+                        </div>
+                        <div class="goods_info_box">
+                            <h2 class="goods_name">ORIGINALS女子 短袖T恤</h2>
+                            <div class="price_box">
+                                <img src="../../images/goods_list/special_price_icon.png" alt="特价icon" class="special_price_tip">
+                                <div class="current_price_box">
+                                    <span class="money_tip">¥</span><span class="price">209.30</span>
+                                </div>
+                                <div class="old_price_box">
+                                    <span class="money_tip">¥</span><span class="price">299.00</span>
+                                </div>
+                            </div>
+                            <div class="date_box">
+                                <i class="date_icon"></i><span class="date">2017-06-09</span>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="goods_item" v-for="">
+                        <div class="img_box">
+                            <img src="../../images/test_img.jpg" alt="商品名字" class="goods_img">
+                        </div>
+                        <div class="goods_info_box">
+                            <h2 class="goods_name">ORIGINALS女子 短袖T恤</h2>
+                            <div class="price_box">
+                                <img src="../../images/goods_list/special_price_icon.png" alt="特价icon" class="special_price_tip">
+                                <div class="current_price_box">
+                                    <span class="money_tip">¥</span><span class="price">209.30</span>
+                                </div>
+                                <div class="old_price_box">
+                                    <span class="money_tip">¥</span><span class="price">299.00</span>
+                                </div>
+                            </div>
+                            <div class="date_box">
+                                <i class="date_icon"></i><span class="date">2017-06-09</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div :class="{transparent: !isMore}">
+                    <div id="pullUp">
+                        <span class="pullUpIcon"></span><span class="pullUpLabel">上拉加载更多</span>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
 </template>
 
 <script>
+
+    import  '../../plugins/iscroll.js';
+
 	export default {
 		data() {
 			return {
@@ -78,22 +136,22 @@
 						text: '精选推荐',
 						isSelected: true,
 						sortType: 0
-					}, 
+					},
 					{
 						text: '上架时间',
 						isSelected: false,
 						sortType: 1
-					}, 
+					},
 					{
 						text: '销量',
 						isSelected: false,
 						sortType: 2
-					}, 
+					},
 					{
 						text: '价格升序',
 						isSelected: false,
 						sortType: 3
-					}, 
+					},
 					{
 						text: '价格降序',
 						isSelected: false,
@@ -108,7 +166,9 @@
 					isShowFilterList: false
 				},
 				propsFilterStr: '',
-				colorFilterStr: ''
+				colorFilterStr: '',
+                isMore: false,
+                myScroll: null,
 			}
 		},
 
@@ -190,7 +250,7 @@
 					}
 
 					$(target).addClass('active').siblings('li').removeClass('active');
-					
+
 				}
 				console.log(this.propsFilterStr, '----------', this.colorFilterStr);
 			},
@@ -236,12 +296,59 @@
 				'catId': this.$route.query.catId || 0,
 				'keyWord': encodeURI(this.$route.query.keyWord || ' '),
 				'currentBrandId': this.$route.query.brandId
-				
 			}, (response) => {
 				let data = response.data;
 				this.colorPropertyListArray = data.ColorPropertyListArray;
 				this.goodPropertyListArray = data.GoodPropertyListArray;
 			});
+
+
+            var pullUpEl,
+                pullUpOffset,
+                generatedCount = 0,
+                tempLoad = 0;
+
+            function pullUpAction () {
+            }
+
+            function loaded() {
+                pullUpEl = document.getElementById('pullUp');
+                pullUpOffset = pullUpEl.offsetHeight;
+                var that = this;
+
+                this.myScroll = new iScroll('goodsListBox', {
+                    useTransition: true,
+                    vScrollbar: false,
+                    onRefresh: function () {
+                        if (pullUpEl.className.match('loading')) {
+                            pullUpEl.className = '';
+                            pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多';
+                        }
+                    },
+                    onScrollMove: function () {
+                        if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+                            pullUpEl.className = 'flip';
+                            pullUpEl.querySelector('.pullUpLabel').innerHTML = '释放加载';
+                            this.maxScrollY = this.maxScrollY;
+                        } else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+                            pullUpEl.className = '';
+                            pullUpEl.querySelector('.pullUpLabel').innerHTML = '上拉加载更多';
+                            this.maxScrollY = pullUpOffset;
+                        }
+                    },
+                    onScrollEnd: function () {
+                        if (pullUpEl.className.match('flip')) {
+                            pullUpEl.className = 'loading';
+                            pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载...';
+                            pullUpAction.bind(that)();  // Execute custom function (ajax call?)
+                        }
+                    }
+                });
+            }
+
+            setTimeout(() => {
+                loaded.bind(this)();
+            }, 1000);
 		}
 	}
 </script>
@@ -265,6 +372,7 @@
 		width: 100%;
 		border-bottom: 1px solid #efefef;
 		z-index: 3;
+        background: #fff;
 	}
 
 	.category_box {
@@ -361,13 +469,30 @@
 
 	.filter_bar {
 		position: fixed;
-		top: 1.4rem;
+		top: calc(1.2rem + 1px);
+        padding-top: .2rem;
 		width: 96%;
 		line-height: 1rem;
 		left: 2%;
-		border: 1px solid #efefef;
+
 		text-align: right;
+        z-index: 3;
+        background: #fff;
 	}
+
+    .filter_bar_box {
+        border: 1px solid #efefef;
+    }
+    .new_good_icon {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        width: 24px;
+        height: 24px;
+        background: url('../../images/goods_list/new_goods_empty_icon.png') center no-repeat;
+        background-size: 100% auto;
+    }
 
 	.sort_box,
 	.filter_box {
@@ -559,4 +684,142 @@
 		top: 55%;
 		color: #7f7f7f;
 	}
+</style>
+
+<style scoped>
+    .goods_list_box {
+        position: relative;
+        width: 96%;
+        height: 100%;
+        margin: 0 auto;
+        overflow: hidden;
+        padding-top: 2.6rem;
+        box-sizing: border-box;
+        background: #fff;
+    }
+
+    .scroller {
+        position: absolute;
+        z-index: 1;
+        width: 100%;
+        min-height: 100%;
+        padding: 0;
+    }
+
+    .goods_list {
+        margin-left: -2%;
+        font-size: 0;
+    }
+
+    .goods_item {
+        margin: 0 auto 30px auto;
+        display: inline-block;
+        vertical-align: middle;
+        font-size: 12px;
+        width: 48%;
+        margin-left: 2%;
+    }
+
+    .transparent {
+        visibility: hidden;
+    }
+
+    #pullUp {
+        line-height: 40px;
+        text-align: center;
+    }
+
+    .pullUpIcon {
+        display: inline-block;
+        vertical-align: middle;
+        width: 0.386473rem;
+        height: 0.386473rem;
+        background: url('../../images/common/load_more.gif') center no-repeat;
+        background-size: 100% auto;
+        margin-right: 0.193237rem;
+    }
+
+    .pullUpLabel {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .img_box {
+        width: 100%;
+        padding-top: 100%;
+        position: relative;
+        overflow: hidden;
+    }
+
+    .goods_img {
+        position: absolute;
+        width: 100%;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .goods_info_box {
+        line-height: 1.5;
+    }
+
+    .special_price_tip {
+        width: 24px;
+    }
+
+    .current_price_box,
+    .old_price_box {
+        display: inline-block;
+        vertical-align: top;
+    }
+
+    .old_price_box {
+        color: #7f7f7f;
+        font-weight: normal;
+    }
+
+    .money_tip {
+        display: inline-block;
+        vertical-align: text-top;
+        font-size: 8px;
+        line-height: 8px;
+    }
+
+    .price {
+        display: inline-block;
+        vertical-align: text-top;
+    }
+
+    .old_price_box .price {
+        text-decoration: line-through;
+        font-size: 13px;
+        line-height: 13px;
+    }
+
+    .current_price_box .price {
+        font-weight: bolder;
+        font-size: 14px;
+        line-height: 14px;
+    }
+
+    .date_box {
+        text-align: right;
+        color: #888;
+    }
+
+    .date_box:before {
+        content: '';
+        display: inline-block;
+        vertical-align: middle;
+        width: 10px;
+        height: 10px;
+        background: url('../../images/goods_list/clock_icon.png') center no-repeat;
+        background-size: 100% auto;
+        margin-right: 3px;
+    }
+
+    .date {
+        display: inline-block;
+        vertical-align: middle;
+    }
 </style>
